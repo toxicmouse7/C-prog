3e378b6325ae0f48d6e37e1e9f94c6abd3c33de4
toxicmouse7 <utiha@sasuke.ru>
2020-03-31 14:52:40 +0500

    Update aud_7_1.c

--- aud_7_1.c
+++ aud_7_1.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-	int i = 0;
-	char ascii[255];
+	int i = 0, lenght = 0;
+	int ascii[255] = { 0 };
 	char* string = (char*)calloc(1, sizeof(char));
 	
 	while (1)
@@ -16,13 +16,42 @@ int main()
 			string[i] = '\0';
 			break;
 		}
+		if (string[i] == '\b')
+		{
+			if (i > 1)
+			{
+				i -= 1;
+				string[i] = '\0';
+				system("cls");
+				printf("%s", string);
+				continue;
+			}
+			if (i <= 1)
+			{
+				i = 0;
+				system("cls");
+				string = (char*)realloc(string, i + 1);
+				continue;
+			}
+		}
 
 		i++;
 		string = (char*)realloc(string, i + 1);
 	}
-
+	string[i] = '\0';
 	system("cls");
-	printf("%s\n", string);
+
+	for (int j = 0; j < strlen(string); j++)
+	{
+		if (ascii[(int)string[j]] == 0)
+		{
+			printf("%c", string[j]);
+			ascii[(int)string[j]] = 1;
+			lenght++;
+		}
+	}
+
+	printf("\n%s\nLenght of substring: %d\n", string, lenght);
 
 	return 0;
 }

82d970b41e4c8724dd0467f4593e6b8e494667f0
toxicmouse7 <utiha@sasuke.ru>
2020-04-05 04:16:34 +0500

    Update snake.c

--- snake.c
+++ snake.c
@@ -11,11 +11,11 @@ typedef struct
 	struct Snake* previous;
 } Snake;
 
-//typedef struct
-//{
-//	int x;
-//	int y;
-//} Apple;
+typedef struct
+{
+	int x;
+	int y;
+} Apple;
 
 void gotoxy(int x, int y)
 {
@@ -45,26 +45,78 @@ void create_game_field()
 
 }
 
-void Add_Random_Apple()
+void Add_Random_Apple(Apple* apple)
 {
-	gotoxy(rand() % 40, rand() % 20);
+	apple->x = rand() % 40;
+	apple->y = rand() % 20;
+	if (apple->x == 0)
+		apple->x++;
+	if (apple->y == 0)
+		apple->y++;
+}
+
+void Output_Apple(Apple* apple)
+{
+	gotoxy(apple->x, apple->y);
 	printf("@");
 }
 
-void Output_Snake()
+void Output_Snake(Snake* snake)
 {
+	while (snake != NULL)
+	{
+		gotoxy(snake->x, snake->y);
+		printf("*");
+		snake = snake->next;
+	}
+}
 
+void increase_snake(Snake* snake, char ch)
+{
+	Snake* tmp;
+	while (snake->next != NULL)
+		snake = snake->next;
+	tmp = snake;
+	snake = snake->next;
+	snake = (Snake*)malloc(sizeof(Snake));
+	snake->next = NULL;
+	snake->previous = tmp;
+	if (ch == 'w')
+	{
+		snake->y = tmp->y + 1;
+		snake->x = tmp->x;
+	}
+	else if (ch == 'a')
+	{
+		snake->y = tmp->y;
+		snake->x = tmp->x + 1;
+	}
+	else if (ch == 's')
+	{
+		snake->y = tmp->y - 1;
+		snake->x = tmp->x;
+	}
+	else if (ch == 'd')
+	{
+		snake->y = tmp->y;
+		snake->x = tmp->x - 1;
+	}
 }
 
 int main()
 {
 	Snake* snake = (Snake*)malloc(sizeof(Snake));
+	Snake* snake_go;
+	Apple* apple = (Apple*)malloc(sizeof(Apple));
+	int score = 0;
 	snake->next = NULL;
-	snake->x = 1;
-	snake->y = 1;
+	snake->x = 20;
+	snake->y = 10;
 	char ch = 'm';
 
 	create_game_field();
+	Add_Random_Apple(apple);
+	Output_Apple(apple);
 
 	while (1)
 	{
@@ -75,27 +127,106 @@ int main()
 		{
 			case 'w': 
 			{
+				snake_go = snake;
+				while (snake_go != NULL)
+				{
+					snake_go->y--;
+					snake_go = snake_go->next;
 
+				}
+				Output_Snake(snake);
+				snake_go = snake;
+				while (snake_go->next != NULL)
+					snake_go = snake->next;
+				if (snake_go->y + 1 != 0)
+				{
+					gotoxy(snake_go->x, snake_go->y + 1);
+					printf(" ");
+					gotoxy(snake_go->x, snake_go->y);
+				}
+				Sleep(300);
+				break;
 			}
 			case 'a':
 			{
+				snake_go = snake;
+				while (snake_go != NULL)
+				{
+					snake_go->x--;
+					snake_go = snake_go->next;
 
+				}
+				Output_Snake(snake);
+				snake_go = snake;
+				while (snake_go->next != NULL)
+					snake_go = snake->next;
+				if (snake_go->x + 1 != 0)
+				{
+					gotoxy(snake_go->x + 1, snake_go->y);
+					printf(" ");
+					gotoxy(snake_go->x, snake_go->y);
+				}
+				Sleep(300);
+				break;
 			}
 			case 's':
 			{
+				snake_go = snake;
+				while (snake_go != NULL)
+				{
+					snake_go->y++;
+					snake_go = snake_go->next;
 
+				}
+				Output_Snake(snake);
+				snake_go = snake;
+				while (snake_go->next != NULL)
+					snake_go = snake->next;
+				if (snake_go->y - 1 != 0)
+				{
+					gotoxy(snake_go->x, snake_go->y - 1);
+					printf(" ");
+					gotoxy(snake_go->x, snake_go->y);
+				}
+				Sleep(300);
+				break;
 			}
 			case 'd':
 			{
+				snake_go = snake;
+				while (snake_go != NULL)
+				{
+					snake_go->x++;
+					snake_go = snake_go->next;
 
+				}
+				Output_Snake(snake);
+				snake_go = snake;
+				while (snake_go->next != NULL)
+					snake_go = snake->next;
+				if (snake_go->x - 1 != 0)
+				{
+					gotoxy(snake_go->x - 1, snake_go->y);
+					printf(" ");
+					gotoxy(snake_go->x, snake_go->y);
+				}
+				Sleep(300);
+				break;
 			}
 		}
 
-		if (snake->x >= 40 || snake->x <= 0 || snake->y <= 0 || snake->y >= 20)
+		if (snake->x >= 39 || snake->x < 1 || snake->y < 1 || snake->y >= 19)
 		{
 			system("cls");
 			printf("Game Over\n");
 			break;
+		}
+
+		if (snake->x == apple->x && snake->y == apple->y)
+		{
+			increase_snake(snake, ch);
+			Add_Random_Apple(apple);
+			Output_Apple(apple);
 		}
 
 	}

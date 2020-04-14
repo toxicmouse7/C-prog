163a2190667ef707efa87bc282791c9ae8e1d16d
toxicmouse7 <utiha@sasuke.ru>
2020-04-15 02:47:31 +0500

    Update aud_9_1.c

--- aud_9_1.c
+++ aud_9_1.c
@@ -6,6 +6,52 @@
 #include <time.h>
 #define INFO_BUFFER_SIZE 50
 
+void wday(struct tm time_date)
+{
+	if (time_date.tm_wday == 0)
+		printf("Понедельник\n");
+	else if (time_date.tm_wday == 1)
+		printf("Вторник\n");
+	else if (time_date.tm_wday == 2)
+		printf("Среда\n");
+	else if (time_date.tm_wday == 3)
+		printf("Четверг\n");
+	else if (time_date.tm_wday == 4)
+		printf("Пятница\n");
+	else if (time_date.tm_wday == 5)
+		printf("Суббота\n");
+	else if (time_date.tm_wday == 6)
+		printf("Воскресение\n");
+}
+
+void month(struct tm time_date)
+{
+	if (time_date.tm_mon == 0)
+		printf("Января");
+	else if (time_date.tm_mon == 1)
+		printf("Февраля");
+	else if (time_date.tm_mon == 2)
+		printf("Марта");
+	else if (time_date.tm_mon == 3)
+		printf("Апреля");
+	else if (time_date.tm_mon == 4)
+		printf("Мая");
+	else if (time_date.tm_mon == 5)
+		printf("Июня");
+	else if (time_date.tm_mon == 6)
+		printf("Июля");
+	else if (time_date.tm_mon == 7)
+		printf("Августа");
+	else if (time_date.tm_mon == 8)
+		printf("Сентября");
+	else if (time_date.tm_mon == 9)
+		printf("Октября");
+	else if (time_date.tm_mon == 10)
+		printf("Ноября");
+	else if (time_date.tm_mon == 11)
+		printf("Декабря");
+}
+
 
 int main()
 {
@@ -25,8 +71,12 @@ int main()
 	bufCharCount = INFO_BUFFER_SIZE;
 
 	localtime_s(&time_date, &long_time);
-	asctime_s(timebuf, 26, &time_date);
-	printf("Время и дата: %s", timebuf);
+	printf("Время: %d:%d:%d\n", time_date.tm_hour, time_date.tm_min, time_date.tm_sec);
+	printf("Дата: %d ", time_date.tm_mday);
+	month(time_date);
+	printf(" %d, ", time_date.tm_year + 1900);
+	wday(time_date);
+	
 
 	GetWindowsDirectory(infoBuf, INFO_BUFFER_SIZE);
 	wprintf(L"Директория Windows: %s\n", infoBuf);
